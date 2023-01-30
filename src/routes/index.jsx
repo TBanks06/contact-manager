f505c93635d1faddd93c003df25169ebcd58db8f
Kazeem Bankole <tunjibankole@gmail.com>
2023-01-30 18:10:48 +0100

    created the index route elements

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+export default function Index(){
+    return (
+            <p id="zero-state">
+                I am me.
+                <br />
+                Check out {''}
+                <a href="https://github,com/TBanks06">
+                    my other portfolios
+                </a>
+            </p>
+    )
+}
